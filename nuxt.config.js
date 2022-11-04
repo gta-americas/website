c5a407f78042d190a636d782bf568a010977b523
Nicxes <nicxesh@gmail.com>
2022-11-04 06:40:29 -0300

    fix(favicon): Added svg favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+		],
+		link: [
+			{ rel: 'icon', href: 'favicon.svg' }
 		]
 	},
 	css: [

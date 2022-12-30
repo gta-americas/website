a28af8acf6d4d442a4b2d5156b6d9aae56266658
Nicxes <nicxesh@gmail.com>
2022-12-29 21:38:09 -0300

    fix(map): Map page fixed

--- pages/map/index.vue
+++ pages/map/index.vue
@@ -2,7 +2,6 @@
   <div class="relative">
     <section class="relative blur-md">
       <Map />
-      <Panel />
     </section>
 
     <div class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
